dd4d578024abd2eb67699060f285c31e43b2c8de
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-06 18:17:23 +0100

    first post request

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import requests
 
-
+pixela_endpoint = "https://pixe.la/v1/users"
+token = ""
+username = "mario"
+user_params = {
+    "token": token,
+    "username": username,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+response = requests.post()(url=pixela_endpoint, json=user_params)
+print(response.text)
 
