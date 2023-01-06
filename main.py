19124f34040288d8523c0194566d446b4b9f91b7
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-07 00:45:48 +0100

    set pixel data

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import requests
 pixela_endpoint = "https://pixe.la/v1/users"
 token = "ajnj24j453n3jkgöhöl64"
 username = "mario"
+graph_id = "graph1"
+pixel_create_endpoint = f"{pixela_endpoint}/{username}/graphs/{graph_id}"
+
 user_params = {
     "token": token,
     "username": username,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
 graph_config = {
@@ -20,5 +24,7 @@ graph_config = {
     "type": "float",
     "color": "sora"
 }
-response = requests.post(url=graph_endpoint, json=graph_config)
+# response = requests.post(url=graph_endpoint, json=graph_config)
+
+pixel_data = {}
 
